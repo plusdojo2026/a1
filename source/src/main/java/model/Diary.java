413d333290d80706bf2dbe86813977957d236dd0
plusdojo2026-horikoshi <horikoshi-rihana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 10:17:14 +0900

    日記登録のDTOとJSPを追加しました。

--- source/src/main/java/model/Diary.java
+++ source/src/main/java/model/Diary.java
@@ -1,7 +1,151 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Diary implements Serializable {
 
+	private int diaryId;
+	private int userId;
+	private Date date;
+	private int weatherCode;
+	private float tempMin;
+	private float tempMax;
+	private String theme; //テーマIDをテーマ名に変換して格納する。
+	private String stamp; //スタンプIDをスタンプのパスに変換して格納する。
+	private String diary;
+	private int satisfaction;
+	private String image;
+	
+	public Diary() {
+		
+	};
+	
+	public Diary(int diaryId, int userId, Date date, int weatherCode, float tempMin, float tempMax, String theme,
+			String stamp, String diary, int satisfaction, String image) {
+		super();
+		this.diaryId = diaryId;
+		this.userId = userId;
+		this.date = date;
+		this.weatherCode = weatherCode;
+		this.tempMin = tempMin;
+		this.tempMax = tempMax;
+		this.theme = theme;
+		this.stamp = stamp;
+		this.diary = diary;
+		this.satisfaction = satisfaction;
+		this.image = image;
+	}
+
+
+	public int getDiaryId() {
+		return diaryId;
+	}
+
+
+	public void setDiaryId(int diaryId) {
+		this.diaryId = diaryId;
+	}
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public int getWeatherCode() {
+		return weatherCode;
+	}
+
+
+	public void setWeatherCode(int weatherCode) {
+		this.weatherCode = weatherCode;
+	}
+
+
+	public float getTempMin() {
+		return tempMin;
+	}
+
+
+	public void setTempMin(float tempMin) {
+		this.tempMin = tempMin;
+	}
+
+
+	public float getTempMax() {
+		return tempMax;
+	}
+
+
+	public void setTempMax(float tempMax) {
+		this.tempMax = tempMax;
+	}
+
+
+	public String getTheme() {
+		return theme;
+	}
+
+
+	public void setTheme(String theme) {
+		this.theme = theme;
+	}
+
+
+	public String getStamp() {
+		return stamp;
+	}
+
+
+	public void setStamp(String stamp) {
+		this.stamp = stamp;
+	}
+
+
+	public String getDiary() {
+		return diary;
+	}
+
+
+	public void setDiary(String diary) {
+		this.diary = diary;
+	}
+
+
+	public int getSatisfaction() {
+		return satisfaction;
+	}
+
+
+	public void setSatisfaction(int satisfaction) {
+		this.satisfaction = satisfaction;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
 }
--- source/src/main/webapp/WEB-INF/jsp/user/diary_regist.jsp
+++ source/src/main/webapp/WEB-INF/jsp/user/diary_regist.jsp
@@ -1,12 +1,99 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<link>
 </head>
-<body>
 
+<body>
+<!-- ヘッダー -->
+<main>
+	<!-- ↓h1全画面共通！↓ -->
+	<h1 class="home">日記の登録</h1>              <!-- ↓調べる -->
+	<form action="/a1/user/diary-regist" method="post" enctype="multipart/form-data">
+	
+		<div>
+			<div>
+				日付天気気温
+			</div>
+		</div>
+		
+		<div>
+			<div>
+				テーマ
+				<select name="theme">
+				<c:foreach var="tm" items="${ <%-- サーブレットで付けた名前が入る --%>}">
+					<option value="${tm.themeId}">${tm.theme}</option><!-- 選ばれたvalueの中身が(番号付けて)情報として送られる -->
+				</c:foreach>
+				</select>
+			</div>
+			<div>
+				スタンプ
+				<div name="stamp">
+					<input type="hidden" name="stamp_id" value="${diary.stampId}"><!-- 送る用のスタンプID -->
+					<div>
+						<img src="${stamp_map[diary.stampId]}" alt="">
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		
+		<div>
+			<div>
+				満足度
+				<input type="hidden" name="satisfaction" value=3>
+			</div>
+		</div>
+		
+		<div>
+			<div>
+				画像<input type="file" name="IMAGE" accept="image/*" onchange="previewImage(this);"><br>
+				<canvas id="preview" ></canvas><br>
+			</div>
+		</div>
+		
+		<div>
+			本文<br><textarea name="diary"></textarea>
+		</div>
+		
+		<div colspan="2">
+			<input type="submit" name="regist" value="登録" ><br>
+		</div>
+		
+	</form>
+	
+</main>
+<!-- フッター -->
 </body>
+<script>
+	function previewImage(obj){
+
+		var fileReader = new FileReader();
+
+		// 読み込み後に実行する処理
+		fileReader.onload = (function() {
+
+			// canvas にプレビュー画像を表示
+			var canvas = document.getElementById('preview');
+			var ctx = canvas.getContext('2d');
+			var image = new Image();
+			image.src = fileReader.result;
+			console.log(fileReader.result) // ← (確認用)
+
+			image.onload = (function () {
+				canvas.width = image.width;
+				canvas.height = image.height;
+				ctx.drawImage(image, 0, 0);
+			});
+		});
+		// 画像読み込み
+		fileReader.readAsDataURL(obj.files[0]);
+		console.log(fileReader.result) // ← (確認用)null
+	}
+</script>
 </html>
