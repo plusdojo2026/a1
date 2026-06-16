56b638bf1044d6fcdf7c3d465e871dfcd3df452f
plusdojo2026-ito <ito-shintaro-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 10:50:58 +0900

    テーマのdtoとjspを追加しました

--- source/src/main/java/model/Theme.java
+++ source/src/main/java/model/Theme.java
@@ -3,5 +3,83 @@ package model;
 import java.io.Serializable;
 
 public class Theme implements Serializable {
+private int themeId;
+private String theme;
+private String stampId;
+private int diaryFlag;
+
+
+public Theme(int themeId, String theme, String stampId, int diaryFlag) {
+	super();
+	this.themeId = themeId;
+	this.theme = theme;
+	this.stampId = stampId;
+	this.diaryFlag = diaryFlag;
+}
+
+
+/**
+ * @return themeId
+ */
+public int getThemeId() {
+	return themeId;
+}
+
+
+/**
+ * @param themeId セットする themeId
+ */
+public void setThemeId(int themeId) {
+	this.themeId = themeId;
+}
+
+
+/**
+ * @return theme
+ */
+public String getTheme() {
+	return theme;
+}
+
+
+/**
+ * @param theme セットする theme
+ */
+public void setTheme(String theme) {
+	this.theme = theme;
+}
+
+
+/**
+ * @return stampId
+ */
+public String getStampId() {
+	return stampId;
+}
+
+
+/**
+ * @param stampId セットする stampId
+ */
+public void setStampId(String stampId) {
+	this.stampId = stampId;
+}
+
+
+/**
+ * @return diaryflag
+ */
+public int getDiaryFlag() {
+	return diaryFlag;
+}
+
+
+/**
+ * @param diaryflag セットする diaryflag
+ */
+public void setDiaryFlag(int diaryflag) {
+	this.diaryFlag = diaryflag;
+}
+
 
 }
--- source/src/main/webapp/WEB-INF/jsp/admin/admin_theme.jsp
+++ source/src/main/webapp/WEB-INF/jsp/admin/admin_theme.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,43 @@
 <title>Insert title here</title>
 </head>
 <body>
+<h1>テーマ</h1>
 
+<button id="openModal">追加</button>
+//追加モーダル
+<div id="modal" class="modal">
+  <div class="modal-content">
+    <span id="closeModal" class="close">&times;</span>
+
+    <p><input type="hidden" value="theme_id">
+　　　　<input type="text">件名<br>
+　　　<textarea>本文</textarea><br>
+　　　　　<input type="submit" value="登録"><br></p>
+  </div>
+</div>
+
+<c:forEach var="tm" items="${themeList}">
+<option>${tm.theme}</option>
+
+//編集モーダル
+<div id="modal" class="modal">
+  <div class="modal-content">
+    <span id="closeModal" class="close">&times;</span>
+   
+    <p>
+    <input type="hidden" value="theme_id">
+<input type="text">件名<br>
+　　　<textarea>本文</textarea><br>
+<input type="submit"value="編集"><br>
+　　　　　</p>
+</div>
+</div>
+</c:forEach>
 </body>
 </html>
+
+
+
+
+
+
