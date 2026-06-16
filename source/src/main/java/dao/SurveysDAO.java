e0dc3ba63a4ac6ef80ed8235c2c463a989d91f10
plusdojo2026-kita <kita-shuto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 14:04:25 +0900

    user_survey.jsp、Survey.java、SurveyDAO.java、UserSurveyServlet.javaの内容を追加しました。

--- source/src/main/java/dao/SurveysDAO.java
+++ source/src/main/java/dao/SurveysDAO.java
@@ -1,5 +1,49 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import model.Survey;
+
 public class SurveysDAO {
 
+	// アンケート登録用メソッド
+	public int insert(Survey survey) {
+		int ans = 0;
+		// データベースに接続と切断を行うオブジェクト
+		Connection conn = null;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL= false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo&connectTimeout=30000",
+					"root", "password");
+			
+			String sql = "INSERT INTO surveys(subject, text) VALUES(?, ?);";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			pStmt.setString(1, survey.getSubject());
+			pStmt.setString(2, survey.getText());
+			
+			ans = pStmt.executeUpdate();
+		
+		}catch (Exception e) {
+			// 例外処理
+			e.printStackTrace();
+			return ans;
+		}finally {
+			// データベースを切断する
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ans;
+	}
 }
--- source/src/main/java/model/Survey.java
+++ source/src/main/java/model/Survey.java
@@ -1,7 +1,59 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Survey implements Serializable {
+	private int surveyId;
+	private String subject;
+	private String text;
+	private Date filledDate;
+	
+	// コンストラクタ
+	public Survey() {
+		
+	};
+	
+	public Survey(int surveyId, String subject, String text, Date filledDate) {
+		this.surveyId = surveyId;
+		this.subject = subject;
+		this.text = text;
+		this.filledDate = filledDate;
+	}
 
+	// 各フィールドのgetterとsetterのメソッド
+	public int getSurveyId() {
+		return surveyId;
+	}
+
+	public void setSurveyId(int surveyId) {
+		this.surveyId = surveyId;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public Date getFilledDate() {
+		return filledDate;
+	}
+
+	public void setFilledDate(Date filledDate) {
+		this.filledDate = filledDate;
+	}
+	
+	
+	
 }
