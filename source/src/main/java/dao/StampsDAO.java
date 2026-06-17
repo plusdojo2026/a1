d347dfdd79bb2053f53bbdfa2ebc08a04fbae674
plusdojo2026-horikoshi <horikoshi-rihana-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-17 13:26:56 +0900

    スタンプDAO

--- source/src/main/java/dao/StampsDAO.java
+++ source/src/main/java/dao/StampsDAO.java
@@ -11,6 +11,60 @@ import java.util.List;
 import model.Stamp;
 
 public class StampsDAO {
+	//全てのスタンプ情報を取得するメソッド
+public List<Stamp> selectAll(){ //引数　stampImg
+		
+		Connection conn = null;//SQLに接続するよ
+		
+		ArrayList<Stamp> stamplist = new ArrayList<Stamp>();
+		
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL= "
+					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/Tokyo&connectTimeout=30000",
+					"root", "password");
+			
+			// SQL文を準備する  変わる情報が入る時は、？で置き換えるよ
+			String sql = "SELECT * FROM stamps ORDER BY stamp_id";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			//pStmt.setString(1, stampImg.getStampPath());  ★一覧表示の時はいらない！（全部持ってくるとき）
+			
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする
+			while (rs.next()) {		
+				Stamp stamp = new Stamp(
+					rs.getInt("stamp_id"),
+					rs.getString("stamp_path")
+				);
+				stamplist.add(stamp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			stamplist = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			stamplist = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					stamplist = null;
+				}
+			}
+		}
+		
+		return stamplist;
+	}
+	
 	//画像一覧表示するメソッド
 	public List<Stamp> select(Stamp stampImg){ //引数　stampImg
 		
