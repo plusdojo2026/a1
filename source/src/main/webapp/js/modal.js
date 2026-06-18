ce2b94a8454ae3fcfc604443d1fb545fd697aece
plusdojo2026-kita <kita-shuto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 17:05:09 +0900

    modal.js修正版

--- source/src/main/webapp/js/modal.js
+++ source/src/main/webapp/js/modal.js
@@ -17,11 +17,17 @@ openBtn.forEach(function(btn){
 		let content3 = btn.dataset.content3;
 		let img = btn.dataset.img;
 		
-		document.getElementById("modal-title").textContent = title;
-		document.getElementById("modal-content1").value = content1;
-		document.getElementById("modal-content2").value = content2;
-		document.getElementById("modal-content3").value = content3;
-		document.getElementById('modal-img').src = img;
+		let modalTitle = document.getElementById("modal-title");
+		let modalContent1 = document.getElementById("modal-content1");
+		let modalContent2 = document.getElementById("modal-content2");
+		let modalContent3 = document.getElementById("modal-content3");
+		let modalImg = document.getElementById('modal-img');
+		
+		if (modalTitle !== null) modalTitle.textContent = title;
+		if (modalContent1 !== null)modalContent1.value = content1;
+		if (modalContent2 !== null)modalContent2.value = content2;
+		if (modalContent3 !== null)modalContent3.value = content3;
+		if (modalImg !== null)modalImg.src = img;
 		
         //.modal-bgにactiveクラスを追加
         modal.classList.add("active");
