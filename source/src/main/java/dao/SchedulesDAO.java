16e604c4471747256bc855932ba4d92a957adcd7
plusdojo2026-kita <kita-shuto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 16:22:13 +0900

    SchedulesDAOにユーザーIDのみで検索するメソッドを追加しました。

--- source/src/main/java/dao/SchedulesDAO.java
+++ source/src/main/java/dao/SchedulesDAO.java
@@ -11,56 +11,50 @@ import java.util.List;
 import model.Schedule;
 
 public class SchedulesDAO {
-	
+
 //	public java.sql.Date convertToSqlDate(LocalDate localDate){
 //		return java.sql.Date.valueOf(localDate);
 //	}
 
-	
-	//予定の一覧表示メソッド
-	public List<Schedule> scList(Schedule sche){
+	// 予定の一覧表示メソッド
+	public List<Schedule> scList(Schedule sche) {
 		Connection conn = null;
 		List<Schedule> resultSche = new ArrayList<Schedule>();
-		
+
 		try {
-			//JDBCドライバの読み込み
+			// JDBCドライバの読み込み
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//データベースに接続
+
+			// データベースに接続
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL="
-					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/"
-					+ "Tokyo&connectTimeout =30000",
-					"root", "password");
-			
-			//SQL文作成 ユーザーIDと日付を基に予定を検索する
+					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/" + "Tokyo&connectTimeout =30000", "root",
+					"password");
+
+			// SQL文作成 ユーザーIDと日付を基に予定を検索する
 			String sql = "SELECT schedule_id, schedule FROM schedules WHERE user_id = ? AND date = ? ORDER BY schedule_id";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
-			//LocalDate→sqlDateの変換
+
+			// LocalDate→sqlDateの変換
 			java.sql.Date sqlDate = java.sql.Date.valueOf(sche.getDate());
-			
+
 			pStmt.setInt(1, sche.getUserId());
 			pStmt.setDate(2, sqlDate);
-			
-			//SQL文実行
+
+			// SQL文実行
 			ResultSet rs = pStmt.executeQuery();
-			
-			//確認用
+
+			// 確認用
 //			System.out.println("SQL文:" + pStmt);
-			
-			//検索結果を格納
+
+			// 検索結果を格納
 			while (rs.next()) {
-				Schedule list = new Schedule(rs.getInt("schedule_id"),
-								0,
-								null,
-								rs.getString("schedule"),
-								null);
+				Schedule list = new Schedule(rs.getInt("schedule_id"), 0, null, rs.getString("schedule"), null);
 				resultSche.add(list);
 			}
-			
-			//確認用
+
+			// 確認用
 //			System.out.println("検索結果:" + resultSche);
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 			resultSche = null;
@@ -68,7 +62,7 @@ public class SchedulesDAO {
 			e.printStackTrace();
 			resultSche = null;
 		} finally {
-			//データベースを切断
+			// データベースを切断
 			if (conn != null) {
 				try {
 					conn.close();
@@ -78,44 +72,99 @@ public class SchedulesDAO {
 				}
 			}
 		}
-		
-		//結果を返す
+
+		// 結果を返す
 		return resultSche;
 	}
-	
-	//予定の登録メソッド
-	public boolean insert(Schedule sche){
+
+	// ユーザーの予定を全取得するメソッド
+	public List<Schedule> selectByUserId(Schedule sche) {
+		Connection conn = null;
+		List<Schedule> resultSche = new ArrayList<Schedule>();
+
+		try {
+			// JDBCドライバの読み込み
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// データベースに接続
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL="
+					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/" + "Tokyo&connectTimeout =30000", "root",
+					"password");
+
+			// SQL文作成 ユーザーIDを基に予定を検索する
+			String sql = "SELECT schedule_id, schedule FROM schedules WHERE user_id = ?;";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			pStmt.setInt(1, sche.getUserId());
+
+			// SQL文実行
+			ResultSet rs = pStmt.executeQuery();
+
+			// 確認用
+//				System.out.println("SQL文:" + pStmt);
+
+			// 検索結果を格納
+			while (rs.next()) {
+				Schedule schedule = new Schedule(rs.getInt("schedule_id"), 0, null, rs.getString("schedule"), null);
+				resultSche.add(schedule);
+			}
+
+			// 確認用
+//				System.out.println("検索結果:" + resultSche);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			resultSche = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			resultSche = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					resultSche = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return resultSche;
+	}
+
+	// 予定の登録メソッド
+	public boolean insert(Schedule sche) {
 		Connection conn = null;
 		boolean result = false;
-		
+
 		try {
-			//JDBCドライバの読み込み
+			// JDBCドライバの読み込み
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//データベースに接続
+
+			// データベースに接続
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL="
-					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/"
-					+ "Tokyo&connectTimeout =30000",
-					"root", "password");
-			
-			//SQL文作成 
+					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/" + "Tokyo&connectTimeout =30000", "root",
+					"password");
+
+			// SQL文作成
 			String sql = "INSERT INTO schedules (user_id, date, schedule) VALUES (?, ?, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
-			//LocalDate→sqlDateの変換
+
+			// LocalDate→sqlDateの変換
 			java.sql.Date sqlDate = java.sql.Date.valueOf(sche.getDate());
-			
+
 			pStmt.setInt(1, sche.getUserId());
 			pStmt.setDate(2, sqlDate);
 			pStmt.setString(3, sche.getSchedule());
-			
-			//SQL文実行
+
+			// SQL文実行
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
 			}
-			
-			
-		}  catch (SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -129,40 +178,38 @@ public class SchedulesDAO {
 				}
 			}
 		}
-		
-		//結果を返す
+
+		// 結果を返す
 		return result;
 	}
-	
-	//予定の編集メソッド
-	public boolean update(Schedule sche){
+
+	// 予定の編集メソッド
+	public boolean update(Schedule sche) {
 		Connection conn = null;
 		boolean result = false;
-		
+
 		try {
-			//JDBCドライバの読み込み
+			// JDBCドライバの読み込み
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//データベースに接続
+
+			// データベースに接続
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL="
-					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/"
-					+ "Tokyo&connectTimeout =30000",
-					"root", "password");
-			
-			//SQL文作成 
+					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/" + "Tokyo&connectTimeout =30000", "root",
+					"password");
+
+			// SQL文作成
 			String sql = "UPDATE schedules SET schedule = ? WHERE schedule_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
+
 			pStmt.setString(1, sche.getSchedule());
 			pStmt.setInt(2, sche.getScheduleId());
-			
-			//SQL文実行
+
+			// SQL文実行
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
 			}
-			
-			
-		}  catch (SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -176,37 +223,36 @@ public class SchedulesDAO {
 				}
 			}
 		}
-		
-		//結果を返す
+
+		// 結果を返す
 		return result;
 	}
-	
-	//予定の削除メソッド
-	public boolean delete(Schedule sche){
+
+	// 予定の削除メソッド
+	public boolean delete(Schedule sche) {
 		Connection conn = null;
 		boolean result = false;
-		
+
 		try {
-			//JDBCドライバの読み込み
+			// JDBCドライバの読み込み
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//データベースに接続
+
+			// データベースに接続
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/a1?useSSL="
-					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/"
-					+ "Tokyo&connectTimeout =30000",
-					"root", "password");
-			
-			//SQL文作成 
+					+ "false&allowPublicKeyRetrieval=true&serverTimezone=Asia/" + "Tokyo&connectTimeout =30000", "root",
+					"password");
+
+			// SQL文作成
 			String sql = "DELETE FROM schedules WHERE schedule_id = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
-			
+
 			pStmt.setInt(1, sche.getScheduleId());
-			
-			//SQL文実行
+
+			// SQL文実行
 			if (pStmt.executeUpdate() == 1) {
 				result = true;
 			}
-		}  catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -220,8 +266,8 @@ public class SchedulesDAO {
 				}
 			}
 		}
-		
-		//結果を返す
+
+		// 結果を返す
 		return result;
 	}
 }
